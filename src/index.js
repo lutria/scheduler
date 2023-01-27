22f811a3ddceb0e72d0574396ec69587590df486
Dave Hughes <djhughes@gmail.com>
2023-01-27 14:24:16 +0000

    Add prettier and eslint

--- src/index.js
+++ src/index.js
@@ -1,47 +1,43 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import cron from "node-cron";
+import pino from "pino";
+import { NatsClient } from "@lutria/nats-common/src/index.js";
+import { scan } from "./scan.js";
 
-import cron from 'node-cron'
-import pino from 'pino'
-import { NatsClient } from '@lutria/nats-common/src/index.js'
-import { scan } from './scan.js'
-
-const logger = pino({ level: process.env.LOG_LEVEL })
+const logger = pino({ level: process.env.LOG_LEVEL });
 
 const natsClient = new NatsClient({
   logger,
-  name: 'scheduler',
+  name: "scheduler",
   servers: process.env.NATS_URL,
-})
+});
 
-await natsClient.connect()
+await natsClient.connect();
 
 const gracefulShutdown = () => {
-  logger.info('Shutting down')
+  logger.info("Shutting down");
 
-  natsClient.disconnect()
-    .finally(() => {
-      process.exit(0)
-    })
-}
+  natsClient.disconnect().finally(() => {
+    process.exit(0);
+  });
+};
 
-process.on('SIGINT', gracefulShutdown)
-process.on('SIGTERM', gracefulShutdown)
+process.on("SIGINT", gracefulShutdown);
+process.on("SIGTERM", gracefulShutdown);
 
-process.on('uncaughtException', (err) => {
+process.on("uncaughtException", (err) => {
   logger.error(err);
   process.exit(1);
 });
 
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   logger.error(err);
   process.exit(1);
 });
 
-cron.schedule('*/10 * * * * *', async () => {
+cron.schedule("*/10 * * * * *", async () => {
   try {
-    await scan(natsClient)
+    await scan(natsClient);
   } catch (e) {
-    logger.error(e)
+    logger.error(e);
   }
-})
+});
--- src/scan.js
+++ src/scan.js
@@ -1,18 +1,22 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import axios from "axios";
+import pino from "pino";
+import { subjects } from "@lutria/nats-common/src/index.js";
 
-import axios from 'axios'
-import pino from 'pino'
-import { subjects } from '@lutria/nats-common/src/index.js'
+const apiServiceUrl = process.env.API_SERVICE_URL;
+const apiUser = process.env.API_USER;
+const authHeader = "x-user";
 
-const apiServiceUrl = process.env.API_SERVICE_URL
-const apiUser = process.env.API_USER
-const authHeader = "x-user"
-
-const logger = pino({ level: process.env.LOG_LEVEL })
+const logger = pino({ level: process.env.LOG_LEVEL });
 
 async function scanStream(natsClient, stream) {
-  const { id: streamId, name, scanCursor, externalId, externalType, security } = stream
+  const {
+    id: streamId,
+    name,
+    scanCursor,
+    externalId,
+    externalType,
+    security,
+  } = stream;
 
   const message = {
     streamId,
@@ -20,33 +24,35 @@ async function scanStream(natsClient, stream) {
     scanCursor,
     externalId,
     externalType,
-    security
-  }
+    security,
+  };
 
-  logger.info(`Sending message to ${subjects.STREAM_SCAN_REQUEST} for stream ${streamId}`)
+  logger.info(
+    `Sending message to ${subjects.STREAM_SCAN_REQUEST} for stream ${streamId}`
+  );
 
-  await natsClient.publish(subjects.STREAM_SCAN_REQUEST, message)
+  await natsClient.publish(subjects.STREAM_SCAN_REQUEST, message);
 }
 
 export async function scan(natsClient) {
-  logger.info("Scanning for stale streams")
+  logger.info("Scanning for stale streams");
 
   // Get list of stale streams from API service
   const response = await axios.get(`${apiServiceUrl}/streams/stale`, {
     headers: {
-      [authHeader]: apiUser
-    }
-  })
+      [authHeader]: apiUser,
+    },
+  });
 
   const streams = response.data;
 
-  logger.debug(`Found ${streams.length} streams`)
+  logger.debug(`Found ${streams.length} streams`);
 
   for (const stream of streams) {
     try {
-      await scanStream(natsClient, stream)
+      await scanStream(natsClient, stream);
     } catch (e) {
-      logger.error(`uh -oh: ${e.message}`)
+      logger.error(`uh -oh: ${e.message}`);
     }
   }
 }
